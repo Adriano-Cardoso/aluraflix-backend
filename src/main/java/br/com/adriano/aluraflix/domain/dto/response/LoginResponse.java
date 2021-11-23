f160c9068639a895e58c8ee077a1904a04fcf0e7
PAMPACOMPASSO\adriano.cardoso <adriano.cardoso@compasso.com.br>
2021-11-23 15:26:10 -0300

    refact: ajuste no response e request

--- src/main/java/br/com/adriano/aluraflix/domain/dto/response/LoginResponse.java
+++ src/main/java/br/com/adriano/aluraflix/domain/dto/response/LoginResponse.java
@@ -1,5 +1,6 @@
 package br.com.adriano.aluraflix.domain.dto.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,8 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class LoginResponse {
 
+	@ApiModelProperty(position = 1, required = false, value = "token do login", name = "token", dataType = "String", example = "asjdj489284u29ur938hd3jd")
 	private String token;
 
+	@ApiModelProperty(position = 2, required = false, value = "tipo do token do login", name = "titulo", dataType = "String", example = "Bearer")
 	private String type;
 
 }
